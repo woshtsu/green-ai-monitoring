839eb42b6d3c2c0b6afdf502815ef902fda07959
woshtsu <ricardoporrasveli@gmail.com>
2026-09-21 10:25:51 -0500

    chore: initialize monitoring service

---
+++ src/main/java/pe/edu/continental/greenai/monitoring/MonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.continental.greenai.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringServiceApplication.class, args);
+	}
+
+}
